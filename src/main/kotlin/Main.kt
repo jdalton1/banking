6432b87a83ed79b72e280cb058822ea27ecc59c7
Josh Dalton <97479607+jdalton1@users.noreply.github.com>
2022-10-01 14:32:10 -0600

    Final edits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import java.util.Scanner
 
+// Main, calls startScreen()
 fun main() {
 
     startScreen()
 }
 
+// The start screen, gets desired action from the user.
 fun startScreen(){
     println("Welcome to Bank of Money!\n")
     println("Are you an existing account holder? (y/n): ")
@@ -17,115 +19,107 @@ fun startScreen(){
     }
 }
 
+// What to output if the user is an account holder
 fun accountHolder() {
     val input = Scanner(System.`in`)
     var balance: Int = 0
-
-    //have database of pin numbers saved
-    //put in loops, add quit option
-    print("Please enter your pin: ")
-    val pin: Int = input.nextInt()
 
     println("Welcome back!\n")
 
     var choice: Int = 0
 
-    while (choice != 7) {
+    while (choice != 4) {
         println("1. Deposit")
         println("2. Withdraw")
-        println("3. Transfer Money")
-        println("4. Check Balance")
-        println("5. Open New Account")
-        println("6. Quit")
+        println("3. Check Balance")
+        println("4. Quit")
 
-        print("What can we do for you today? (1-6) \n")
+        print("What can we do for you today? (1-4) \n")
         choice = input.nextInt()
 
-        // make these into functions
-        // also add choice of which account to deposit/withdraw from
         if (choice == 1) {
             print("Enter desired amount to deposit: ")
             val amountToDeposit: Int = input.nextInt()
 
             balance = balance.plus(amountToDeposit)
-            println("Your new balance is: $balance")
+            println("Your new balance is: $balance\n")
         }
 
         if (choice == 2) {
             print("Enter desired amount to withdraw: ")
             val amountToWithdraw: Int = input.nextInt()
 
-            println("Your new balance is: $balance")
+            balance = balance.minus(amountToWithdraw)
+
+            println("Your new balance is: $balance\n")
         }
 
         if (choice == 3) {
-            print("Which account would you like to transfer money from? ")
-            val transferFromAccount = readLine()
-
-            print("Which account would you like to transfer money to? ")
-            val transferToAccount = readLine()
-
-            print("How much would you like to transfer? ")
-            val amountToTransfer: Int = input.nextInt()
-
-            println("Your new balance is: $balance")
+            println("Your balance is \$$balance.\n")
         }
 
-        if (choice == 4) {
-            println("Your balance is \$$balance.")
-        }
-
-        if (choice == 5){
-            return
-        }
-
-        if (choice == 6){
+        if (choice == 4){
             println("Goodbye!")
             return
         }
+
+        if (choice > 4 || choice < 1) {
+            print("Sorry, invalid option! Try again.")
+            choice = 0
+        }
     }
 }
+
+// Get user information and create an Object of that person using a class
 fun newAccountHolder(){
-    val input = Scanner(System.`in`)
 
     println("Thank you for choosing Bank of Money!")
     println("Let's get some basic information about you.\n")
 
-    print("How old are you? ")
-    val age:Int = input.nextInt()
+    val newUser = Person()
 
-    if (age < 18){
+    newUser.getAge()
+    val firstName = newUser.getFirstName()
+    newUser.getLastName()
+
+    print("Welcome to the Bank of Money, $firstName!")
+
+    accountHolder()
+
+}
+
+
+// Person class, gets and stores information about a person.
+open class Person {
+
+    // Get the users age
+    fun getAge(): Int {
+    val input = Scanner(System.`in`)
+
+    print("How old are you? ")
+    val age: Int = input.nextInt()
+
+    if (age < 18) {
         println("Sorry, you are not old enough to open a bank account!")
-        main()
+        startScreen()
+        return 0
     }
 
-    print("What is your first name? ")
-    val firstName = readLine()
+    else {
+        return age
+        }
+    }
 
-    print("What is your last name? ")
-    val lastName = readLine()
+    // Get the users first name
+    fun getFirstName(): String? {
+        print("What is your first name? ")
+        return readLine()
+    }
 
-    print("What type of account would you like to open? (Checking/Savings) ")
-    val accountType = readLine()
-
-
-}
-
-
-
-
-
-
-
-
-class Person(val firstName: String, val lastName: String, val age: Int){
-
-}
-
-class AccountHolder(val accountNumber: Int, val pin: Int){
-
-}
-
-class Account(){
+    // Get the users last name
+    fun getLastName(): String? {
+        print("What is your last name? ")
+        return readLine()
+    }
 
 }
